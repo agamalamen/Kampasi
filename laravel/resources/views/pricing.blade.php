87c4dc532546ea20d2091a0b281a14e1fe051f55
Ahmed Gamal Amin Mohamed Farag <AFarag16@alastudents.org>
2017-03-17 08:16:23 +0200

    pricing page

---
+++ laravel/resources/views/pricing.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+@section('title') Packages @endsection
+
+<style>
+  h2 {
+    font-size: 34px;
+  }
+
+  h3 {
+    font-size: 24px;
+  }
+
+  h4 {
+    font-size: 16px !important;
+    padding-top: 5px;
+  }
+</style>
+
+<div class="container">
+  <h1 class="text-center" style="font-family: lato;">Kampasi pricing</h1>
+
+  <h2 style="font-size: 34px;">Basic</h2>
+  <div class="row">
+    <div class="col-md-6">
+      <h3><i class="fa fa-gift fa-lg" aria-hidden="true"></i> Birthdays</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div><!-- .col-md-6 -->
+    <div class="col-md-3">
+      <h4><i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard</h4>
+      <p>Education . Passion . Shipping</p>
+      <h4><i class="fa fa-hourglass-half" aria-hidden="true"></i> Prep signup</h4>
+      <p>Education . Passion . Shipping</p>
+      <h4><i class="fa fa-bed" aria-hidden="true"></i> Dorms Management</h4>
+      <p>Education . Passion . Shipping</p>
+    </div><!-- .col-md-3 -->
+    <div class="col-md-3">
+      <h4><i class="fa fa-cubes" aria-hidden="true"></i> Do something cool</h4>
+      <p>Education . Passion . Shipping</p>
+      <h4><i class="fa fa-gift fa-lg" aria-hidden="true"></i> Birthdays</h4>
+      <p>Education . Passion . Shipping</p>
+      <h4><i class="fa fa-comments" aria-hidden="true"></i> Feedback system</h4>
+      <p>Education . Passion . Shipping</p>
+    </div><!-- .col-md-3 -->
+  </div><!-- .row -->
+
+</div><!-- .container -->
